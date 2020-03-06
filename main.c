7e72516b0ef3aa2d258b2a17cbc46c1957d37071
ggjulio <juligonz@student.42.fr>
2020-03-06 17:59:23 +0100

    test mlx mms

---
+++ main.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/06 17:33:39 by juligonz          #+#    #+#             */
+/*   Updated: 2020/03/06 17:55:44 by juligonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "stdio.h"
+#include "inttypes.h"
+
+int t(void)
+{
+    printf("ok\n");
+    return (0);
+}
+
+typedef	struct	s_rgba
+{
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+	uint8_t a;
+}				t_rgba;
+
+typedef union	u_color
+{
+	uint32_t	c;
+	t_rgba		rgba;
+}				t_color;
+
+int main(void)
+{
+
+    void    *mlx_ptr = mlx_init();
+    void *win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "432432");
+
+    t_color color;
+
+    color.c = 0;
+    color.rgba.r = 255;
+
+    int i = 0;
+    int j = 0;
+    while (i < 1000)
+    {
+        j = 0;
+        while (j < 1000)
+        {
+        	mlx_pixel_put(mlx_ptr, win_ptr, j, i, color.c); 
+            j++;
+        }
+        i++;
+    }
+    
+
+
+	mlx_loop_hook (mlx_ptr, t, NULL);
+
+     mlx_loop (mlx_ptr);
+     return (0);
+}
